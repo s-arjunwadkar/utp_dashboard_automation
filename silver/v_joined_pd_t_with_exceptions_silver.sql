9eecac6f0a27732b74cfd66eeea4bacb194b533e
UTP User <utp@example.com>
2026-08-04 12:32:45 -0400

    Cat 8 missing logic added

--- silver/v_joined_pd_t_with_exceptions_silver.sql
+++ silver/v_joined_pd_t_with_exceptions_silver.sql
@@ -22,7 +22,7 @@ normal_exceptions_union AS (
     ORDER BY category, district_mpo_division, fy
 ),
 
-targets_by_district_4r_12_10_11s AS (
+targets_by_district_4r_8_12_10_11s AS (
     SELECT
         category,
         district_mpo_division,
@@ -31,6 +31,7 @@ targets_by_district_4r_12_10_11s AS (
         CASE
             WHEN category = '4R' AND district_mpo_division NOT ILIKE 'Statewide%' THEN total_authorized_amount
             WHEN category IN ('12CL', '12TTC') AND district_mpo_division NOT ILIKE '12%' THEN total_authorized_amount
+            WHEN category IN ('8SF', '8RX') AND district_mpo_division NOT ILIKE '8%' THEN total_authorized_amount
             WHEN category IN ('10ADA', '10FB', '10GR', '10LIA', '10RGC', '10RGS', '10TPW', '10SRATP', '10SCP', '10IDA', '10EAR', '10OTHER') AND district_mpo_division NOT ILIKE '10 - %' THEN total_authorized_amount
             WHEN category = '11B' AND district_mpo_division NOT ILIKE 'Rider%' THEN total_authorized_amount
             WHEN category = '11CO' AND district_mpo_division NOT ILIKE '%Overruns%' THEN total_authorized_amount
@@ -40,14 +41,15 @@ targets_by_district_4r_12_10_11s AS (
     FROM normal_exceptions_union
 ),
 
-target_totals_4r_12_10_11s AS (
+target_totals_4r_8_12_10_11s AS (
     SELECT
         category,
         fy,
         SUM(total_targets) AS new_targets_yearly
-    FROM targets_by_district_4r_12_10_11s
+    FROM targets_by_district_4r_8_12_10_11s
     WHERE (category = '4R' AND district_mpo_division NOT ILIKE 'Statewide%')
         OR (category IN ('12CL', '12TTC') AND district_mpo_division NOT ILIKE '12%')
+        OR (category IN ('8SF', '8RX') AND district_mpo_division NOT ILIKE '8%')
         OR (category IN ('10ADA', '10FB', '10GR', '10LIA', '10RGC', '10RGS', '10TPW', '10SRATP', '10SCP', '10IDA', '10EAR', '10OTHER') AND district_mpo_division NOT ILIKE '10 - %') -- Check here if fails due to '10-' / '10 -' etc. Basically space issue
         OR (category = '11B' AND district_mpo_division NOT ILIKE 'Rider%')
         OR (category = '11CO' AND district_mpo_division NOT ILIKE '%Overruns%')
@@ -55,7 +57,7 @@ target_totals_4r_12_10_11s AS (
     ORDER BY category, fy
 ),
 
-adjust_4r_12_10_11s AS (
+adjust_4r_8_12_10_11s AS (
     SELECT
         m.category,
         m.district_mpo_division,
@@ -67,14 +69,16 @@ adjust_4r_12_10_11s AS (
             WHEN district_mpo_division ILIKE 'Statewide%' AND m.category = '4R' AND (m.category = s.category) AND (m.fy = s.fy) THEN m.total_targets - s.new_targets_yearly
             WHEN district_mpo_division = '12 - Strategic Priority' AND m.category = '12TTC' AND (m.category = s.category) AND (m.fy = s.fy) THEN m.total_targets - s.new_targets_yearly
             WHEN district_mpo_division = '12 - Texas ClearLanes' AND m.category = '12CL' AND (m.category = s.category) AND (m.fy = s.fy) THEN m.total_targets - s.new_targets_yearly
+            WHEN district_mpo_division = '8 - Safety' AND m.category = '8SF' AND (m.category = s.category) AND (m.fy = s.fy) THEN m.total_targets - s.new_targets_yearly
+            WHEN district_mpo_division = '8 - Rail' AND m.category = '8RX' AND (m.category = s.category) AND (m.fy = s.fy) THEN m.total_targets - s.new_targets_yearly
             WHEN district_mpo_division ILIKE '10 - %' AND m.category IN ('10ADA', '10FB', '10GR', '10LIA', '10RGC', '10RGS', '10TPW', '10SRATP', '10SCP', '10IDA', '10EAR', '10OTHER') AND (m.category = s.category) AND (m.fy = s.fy) THEN m.total_targets - s.new_targets_yearly -- Check here if fails due to '10-' / '10 -' etc. Basically space issue
             WHEN district_mpo_division ILIKE 'Rider%' AND m.category = '11B' AND (m.category = s.category) AND (m.fy = s.fy) THEN m.total_targets - s.new_targets_yearly
             WHEN district_mpo_division ILIKE '%Overruns%' AND m.category = '11CO' AND (m.category = s.category) AND (m.fy = s.fy) THEN m.total_targets - s.new_targets_yearly
             ELSE m.total_targets
         END AS total_targets_new,
         m.carryovers
-    FROM targets_by_district_4r_12_10_11s AS m
-    LEFT JOIN target_totals_4r_12_10_11s AS s
+    FROM targets_by_district_4r_8_12_10_11s AS m
+    LEFT JOIN target_totals_4r_8_12_10_11s AS s
     ON m.category = s.category AND m.fy = s.fy
 )
 
@@ -86,6 +90,6 @@ SELECT
     total_targets_new AS total_targets,
     carryovers,
     total_targets_new + carryovers AS targets_carryovers_combined
-FROM adjust_4r_12_10_11s
+FROM adjust_4r_8_12_10_11s
 ORDER BY category, district_mpo_division, fy
 ;
